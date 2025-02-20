a479a6ee8cfffe73d8f0fd44406675aa4e96bb62
Masahiro Toma <143092368+mshr1-create@users.noreply.github.com>
2025-02-20 21:04:26 +0900

    idを修正

--- intern-book-app/src/app/pages/book-list/book-list.component.ts
+++ intern-book-app/src/app/pages/book-list/book-list.component.ts
@@ -23,7 +23,7 @@ export class BookListComponent implements OnInit {
     // 仮の書籍データ
     this.books = [
       { id: 1, title: 'アンドロイドは電気羊の夢を見るか？', description: '第三次大戦後の未来...', rating: 90 },
-      { id: 1, title: '岩田さん：岩田聡はこんなことを話していた。', description: '元任天堂社長の...', rating: 80 },
+      { id: 2, title: '岩田さん：岩田聡はこんなことを話していた。', description: '元任天堂社長の...', rating: 80 },
     ]
   }
 
