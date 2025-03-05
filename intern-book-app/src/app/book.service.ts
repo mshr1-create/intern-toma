6218848c7d66b9f9f737fee52df062ad3a5e7316
Masahiro Toma <143092368+mshr1-create@users.noreply.github.com>
2025-03-05 17:31:23 +0900

    書籍情報の追加#7

---
+++ intern-book-app/src/app/book.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Book } from './types/book';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookService {
+  // 書籍データを格納する BehaviorSubject
+  private booksSubject = new BehaviorSubject<Book[]>([]);
+  books$ = this.booksSubject.asObservable();
+
+  constructor() {
+    // 仮の書籍データ
+    const initialBooks: Book[] = [
+      { id: 1, title: 'アンドロイドは電気羊の夢を見るか？', description: '第三次大戦後の未来...', rating: 90 },
+      { id: 2, title: '岩田さん：岩田聡はこんなことを話していた。', description: '元任天堂社長の...', rating: 80 },
+    ];
+    this.booksSubject.next(initialBooks);
+  }
+
+  
+  addBook(book: Book): void {
+    const currentBooks = this.booksSubject.getValue();
+    this.booksSubject.next([...currentBooks, book]);
+  }
+}
--- intern-book-app/src/app/pages/book-resister/book-register.component.ts
+++ intern-book-app/src/app/pages/book-resister/book-register.component.ts
@@ -4,6 +4,8 @@ import { MatButton } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';;
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { BookService } from '../../book.service';
+import { Book } from '../../types/book';
 
 @Component({
   standalone : true,
@@ -16,14 +18,29 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     MatFormFieldModule
   ],
   templateUrl: './book-register.component.html',
-  styleUrls: ['./book-register.component.css']
+  styleUrl: './book-register.component.css'
 })
 export class BookRegisterComponent {
   bookTitle: string = '';
   bookDescription: string = '';
   bookRating: number | null = null;
 
+  constructor(private bookService: BookService) {}
+  
   onAddBook(): void {
+    // 新しい書籍情報を定義
+    const newBook: Book = {
+      id: Date.now(), // 現在のタイムスタンプをIDとして使用
+      title: this.bookTitle,
+      description: this.bookDescription,
+      rating: this.bookRating ?? 0
+    };
 
-  }
+    this.bookService.addBook(newBook);
+
+    // 入力フォームをリセット
+    this.bookTitle = '';
+    this.bookDescription = '';
+    this.bookRating = null;
+  } 
 }
